bfda6a1187c3d222896a2116b993dce1a85621b0
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-10-06 21:39:47 -0300

    🎨 Changing grid-item from div to button

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -10,7 +10,7 @@ const Grid = (props) => {
     <div className="grid-container">
         {matrix.map((row, i) => {
             return row.map((e, j) => {
-                return <div id={`(${i}-${j})`} key={j} className={e !== 0 ? "grid-item active" : "grid-item"}></div>
+                return <button id={`(${i}-${j})`} key={j} className={e !== 0 ? "grid-item active" : "grid-item"}></button>
             })
         })}
     </div>
